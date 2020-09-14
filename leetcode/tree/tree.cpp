6647d386f6930414c23a113299011c89ccc00478
hl4080 <hl4080@foxmail.com>
2020-09-14 19:04:47 +0800

    feat: leecode-Populating Next Right Pointers in Each Node

--- leetcode/tree/tree.cpp
+++ leetcode/tree/tree.cpp
@@ -560,13 +560,39 @@ The flattened tree should look like:
  */
 
 void flattenTree(TreeNode* root) {
-    if(!root || !root->left) return;
-    TreeNode* l = root->left;
-    TreeNode* r = root->right;
-    root->left = nullptr;
-    root->right = l;
-    while(l->right) l = l->right;
-    l->right = r;
-    flattenTree(root->left);
-    flattenTree(root->right);
+    TreeNode* node = root;
+    while(node) {
+        if(node->left) {
+            TreeNode* l = node->left;
+            TreeNode* r = node->right;
+            node->right = l;
+            while(l->right) l = l->right;
+            l->right = r;
+            node->left = nullptr;
+        }
+        node = node->right;
+    }
 }
+
+/*
+ * You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate
+each next pointer to point to its next right node, just like in Figure B.
+The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+
+ */
+
+//see more details on leetcode
